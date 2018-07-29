c831f83b84423560daea6a262c5125430e807e9b
Kiril Vatev <vatev.1@gmail.com>
2018-07-29 15:13:08 -0400

    loading an image from drag-drop and displaying the image

--- public/dropzone.js
+++ public/dropzone.js
@@ -2,13 +2,36 @@
 
 console.log('the script loaded');
 
+// TODO move this stuff to the main thread
+const fs = require('fs');
+const dcraw = require('dcraw');
+
 const dropArea = document.querySelector('#dropzone');
+const image = document.querySelector('#image');
 
-function handlerFunction(ev) {
-  ev.preventDefault();
-  ev.stopPropagation();
+function loadImage(path) {
+  console.time('read');
+  const file = fs.readFileSync(path);
+  console.timeEnd('read');
 
-  console.log(ev);
+  console.time('meta');
+  var meta = dcraw(file, { verbose: true, identify: true });
+  console.timeEnd('meta');
+
+  console.log(meta);
+
+  console.time('tiff');
+  var tiff = dcraw(file, { extractThumbnail: true });
+//  var tiff = dcraw(file, { exportAsTiff: true });
+  console.timeEnd('tiff');
+
+  console.log(tiff.length);
+
+  var img = document.createElement('img');
+  img.src = 'data:image/jpeg;base64,' + Buffer.from(tiff).toString('base64');
+
+  image.innerHTML = '';
+  image.appendChild(img);
 }
 
 function stop() {
@@ -25,7 +48,9 @@ dropArea.ondrop = (e) => {
   console.log(e);
 
   for (let f of e.dataTransfer.files) {
-    console.log('File(s) you dragged here: ', f.path)
+    console.log('File(s) you dragged here: ', f.path);
+
+    loadImage(f.path);
   }
 
   return false;

da81af1f9b2d7bc1afcc72605a8ba61f24d81b97
Kiril Vatev <vatev.1@gmail.com>
2018-08-24 14:49:54 -0400

    setting ratings by clicking on stars

--- renderer/filmstrip/rating.js
+++ renderer/filmstrip/rating.js
@@ -1,17 +1,57 @@
-const ratings = {
-  '0': '☆☆☆☆☆',
-  '1': '★☆☆☆☆',
-  '2': '★★☆☆☆',
-  '3': '★★★☆☆',
-  '4': '★★★★☆',
-  '5': '★★★★★'
-};
+const log = require('../../tools/log.js')('filmstrip-rating');
+const exiftool = require('../exiftool-child.js');
+
+function ratingControl(rating) {
+  function star(setTo, isFull = false) {
+    const el = document.createElement('span');
+    el.setAttribute('data-rate', setTo);
+    el.appendChild(document.createTextNode(isFull ? '★' : '☆'));
+
+    return el;
+  }
+
+  return Array.apply(null, new Array(5)).map((n, i) => {
+    return star(i + 1, i + 1 <= rating);
+  });
+}
+
+async function setRating(filepath, rating) {
+  return await exiftool.setRating(filepath, rating);
+}
+
+function render(elem, filepath, rating) {
+  elem.innerHTML = '';
+
+  const fragment = document.createDocumentFragment();
+
+  ratingControl(rating).forEach((el, idx) => {
+    el.addEventListener('click', (ev) => {
+      console.log('rating click', ev.target);
+      ev.preventDefault();
+
+      setRating(filepath, idx + 1).then(data => {
+        log.info('rating response', data);
+
+        render(elem, filepath, idx + 1);
+      }).catch(err => {
+        log.error(err);
+      });
+
+      return false;
+    });
+
+    fragment.appendChild(el);
+  });
+
+  elem.appendChild(fragment);
+}
 
 module.exports = function ({ filepath, meta }) {
   const elem = document.createElement('div');
   elem.className = 'rating';
+  elem.setAttribute('data-rating', meta.rating);
 
-  elem.appendChild(document.createTextNode(ratings[meta.rating || 0]));
+  render(elem, filepath, meta.rating);
 
   return elem;
 };

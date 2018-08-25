2333716b6492d0feab8ece82b76bd8c589a09453
Kiril Vatev <vatev.1@gmail.com>
2018-08-25 13:48:32 -0400

    splitting out image manipulation controls to separate file

---
+++ renderer/image/image-control.js
@@ -0,0 +1,135 @@
+const log = require('../../tools/log.js')('image-control');
+const keys = require('../tools/keyboard.js');
+
+function int(num) {
+  return Math.floor(num);
+}
+
+function evenInt(num) {
+  const i = int(num);
+
+  return i % 2 ? i - 1 : i;
+}
+
+module.exports = ({ name, elem }) => {
+  let box;
+  let scale = 1;
+  let width = 0;
+  let height = 0;
+  let isRotated = false;
+  let rotateStyle = '';
+
+  const img = document.createElement('img');
+  const container = document.createElement('div');
+  container.className = `${name}-container`;
+
+  container.appendChild(img);
+
+  function refreshBox() {
+    const rect = elem.getBoundingClientRect();
+
+    box = {
+      width: rect.width - 20,
+      height: rect.height - 20
+    };
+  }
+
+  refreshBox();
+
+  window.addEventListener('resize', () => {
+    // TODO don't do this on every single event
+    refreshBox();
+  });
+
+  function zoomToBestFit() {
+    const scale = Math.min(
+      box.width / width,
+      box.height / height
+    );
+
+    zoom(scale);
+  }
+
+  function zoom(toScale) {
+    if (toScale === 'fit') {
+      return zoomToBestFit();
+    }
+
+    // JavaScript math sucks
+    scale = Math.min(Number(toScale.toFixed(2)), 1);
+
+    log.info('zoom to', scale);
+
+    const targetWidth = evenInt(width * scale);
+    const targetHeight = evenInt(height * scale);
+
+    const transformWidth = targetWidth > box.width ?
+      int((targetWidth / 2) - (box.width / 2)) : 0;
+
+    const transformHeight = targetHeight > box.height ?
+      int((targetHeight / 2) - (box.height / 2)) : 0;
+
+    if (isRotated) {
+      img.style.width = `${targetHeight}px`;
+      img.style.height = `${targetWidth}px`;
+      img.style.transform = rotateStyle;
+    }  else {
+      img.style.width = `${targetWidth}px`;
+      img.style.height = `${targetHeight}px`;
+    }
+
+    container.style.width = `${targetWidth}px`;
+    container.style.height = `${targetHeight}px`;
+    container.style.transform = `translate(${transformWidth}px, ${transformHeight}px)`;
+
+    // scroll to center by default
+    elem.scrollTop = (targetHeight / 2) - (box.height / 2);
+    elem.scrollLeft = (targetWidth / 2) - (box.width / 2);
+  }
+
+  function load({ imageUrl, rotation }) {
+    width = img.width;
+    height = img.height;
+    rotateStyle = rotation === 0 ? '' : `rotate(${rotation}deg)`;
+    isRotated = rotation === 90 || rotation === 270;
+
+    refreshBox();
+
+
+    img.onload = function () {
+      if (isRotated) {
+        width = img.naturalHeight;
+        height = img.naturalWidth;
+      } else {
+        width = img.naturalWidth;
+        height = img.naturalHeight;
+      }
+
+      zoomToBestFit();
+    };
+
+    elem.onclick = function () {
+      const zout = keys.includes(keys.ALT) && keys.includes('z');
+      const zin = keys.includes('z') && !zout;
+
+      if (scale > 0.1 && zout) {
+        // zooming out
+        scale -= 0.1;
+        zoom(scale);
+      } else if (scale < 1 && zin) {
+        // zooming in
+        scale += 0.1;
+        zoom(scale);
+      }
+    };
+
+    img.style.transform = '';
+    img.src = imageUrl;
+  }
+
+  return {
+    dom: container,
+    load,
+    zoom
+  };
+};
--- renderer/image/image.js
+++ renderer/image/image.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-const log = require('../../tools/log.js')('image');
 const keys = require('../tools/keyboard.js');
 
 const name = 'image';
@@ -61,31 +60,9 @@ module.exports = function ({ events }) {
   const elem = document.createElement('div');
   elem.className = name;
 
-  const container = document.createElement('div');
-  container.className = `${name}-container`;
+  const { dom, load, zoom } = require('./image-control.js')({ name, elem });
 
-  const img = document.createElement('img');
-
-  container.appendChild(img);
-  elem.appendChild(container);
-
-  let box;
-
-  function refreshBox() {
-    const rect = elem.getBoundingClientRect();
-
-    box = {
-      width: rect.width - 20,
-      height: rect.height - 20
-    };
-  }
-
-  refreshBox();
-
-  window.addEventListener('resize', () => {
-    // TODO don't do this on every single event
-    refreshBox();
-  });
+  elem.appendChild(dom);
 
   keys.on('change', ({ down }) => {
     if (down.includes(keys.SPACE)) {
@@ -103,101 +80,11 @@ module.exports = function ({ events }) {
     return elem.style.cursor = 'auto';
   });
 
-  function loadImage({ imageUrl, rotation }) {
-    let scale = 1;
-    let width = img.width;
-    let height = img.height;
-    const rotate = rotation === 0 ? '' : `rotate(${rotation}deg)`;
-    const isRotated = rotation === 90 || rotation === 270;
-
-    refreshBox();
-
-    function int(num) {
-      return Math.floor(num);
-    }
-
-    function evenInt(num) {
-      const i = int(num);
-
-      return i % 2 ? i - 1 : i;
-    }
-
-    function zoom(toScale) {
-      // JavaScript math sucks
-      scale = Math.min(Number(toScale.toFixed(2)), 1);
-
-      log.info('zoom to', scale);
-
-      const targetWidth = evenInt(width * scale);
-      const targetHeight = evenInt(height * scale);
-
-      const transformWidth = targetWidth > box.width ?
-        int((targetWidth / 2) - (box.width / 2)) : 0;
-
-      const transformHeight = targetHeight > box.height ?
-        int((targetHeight / 2) - (box.height / 2)) : 0;
-
-      if (isRotated) {
-        img.style.width = `${targetHeight}px`;
-        img.style.height = `${targetWidth}px`;
-        img.style.transform = rotate;
-      }  else {
-        img.style.width = `${targetWidth}px`;
-        img.style.height = `${targetHeight}px`;
-      }
-
-      container.style.width = `${targetWidth}px`;
-      container.style.height = `${targetHeight}px`;
-      container.style.transform = `translate(${transformWidth}px, ${transformHeight}px)`;
-
-      // scroll to center by default
-      elem.scrollTop = (targetHeight / 2) - (box.height / 2);
-      elem.scrollLeft = (targetWidth / 2) - (box.width / 2);
-    }
-
-    function zoomToBestFit() {
-      const scale = Math.min(
-        box.width / width,
-        box.height / height
-      );
-
-      zoom(scale);
-    }
-
-    img.onload = function () {
-      if (isRotated) {
-        width = img.naturalHeight;
-        height = img.naturalWidth;
-      } else {
-        width = img.naturalWidth;
-        height = img.naturalHeight;
-      }
-
-      zoomToBestFit();
-    };
-
-    elem.onclick = function () {
-      const zout = keys.includes(keys.ALT) && keys.includes('z');
-      const zin = keys.includes('z') && !zout;
-
-      if (scale > 0.1 && zout) {
-        // zooming out
-        scale -= 0.1;
-        zoom(scale);
-      } else if (scale < 1 && zin) {
-        // zooming in
-        scale += 0.1;
-        zoom(scale);
-      }
-    };
-
-    img.style.transform = '';
-    img.src = imageUrl;
-  }
-
   registerMouse(elem);
 
-  events.on('load:image', loadImage);
+  events.on('image:zoom', ({ scale }) => zoom(scale));
+
+  events.on('load:image', ({ imageUrl, rotation }) => load({ imageUrl, rotation }));
 
   events.on('load:image', ({ filepath }) => {
     events.emit('load:meta', {

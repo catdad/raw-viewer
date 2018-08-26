cfc44fb47000c423823c976affc0fd6092e21362
Kiril Vatev <vatev.1@gmail.com>
2018-08-26 11:44:37 -0400

    improving styling for dropzone

--- renderer/dropzone/dropzone.js
+++ renderer/dropzone/dropzone.js
@@ -4,10 +4,25 @@ const path = require('path');
 const name = 'dropzone';
 const style = fs.readFileSync(path.resolve(__dirname, `${name}.css`), 'utf8');
 
+function dropzoneContent() {
+  const container = document.createElement('div');
+  container.className = 'container';
+
+  const text = document.createElement('div');
+  text.className = 'text';
+
+  text.appendChild(document.createTextNode('drag and drop a folder to view'));
+  container.appendChild(text);
+
+  return container;
+}
+
 module.exports = function ({ events }) {
   var elem = document.createElement('div');
   elem.className = name;
   let hasDir = false;
+
+  elem.appendChild(dropzoneContent());
 
   function stop() {
     return false;

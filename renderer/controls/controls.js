a4a68632344246a74e8eecedaef08f78f2a3cb11
Kiril Vatev <vatev.1@gmail.com>
2018-08-30 21:38:51 -0400

    adding rating filter control to the UI

--- renderer/controls/controls.js
+++ renderer/controls/controls.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const name = 'controls';
 const style = fs.readFileSync(path.resolve(__dirname, `${name}.css`), 'utf8');
+let curentRating = 0;
 
 function button({ onclick, text, className = '' }) {
   const el = document.createElement('button');
@@ -11,6 +12,41 @@ function button({ onclick, text, className = '' }) {
   el.addEventListener('click', onclick);
 
   return el;
+}
+
+function stars({ onclick, className = '' }) {
+  const parent = document.createElement('span');
+  parent.className = className;
+
+  function setFilter({ rating }) {
+    curentRating = rating;
+    onclick({ rating });
+
+    spans.forEach((span, idx) => {
+      if (idx === 0) {
+        return;
+      }
+
+      span.innerText = idx <= curentRating ? '★' : '☆';
+    });
+  }
+
+  const spans = [0, 1, 2, 3, 4, 5].map((rating) => {
+    const char = rating === 0 ? '⨯' :
+      rating <= curentRating ? '★' : '☆';
+
+    const el = document.createElement('span');
+    el.onclick = () => {
+      setFilter({ rating });
+    };
+    el.appendChild(document.createTextNode(char));
+
+    parent.appendChild(el);
+
+    return el;
+  });
+
+  return parent;
 }
 
 module.exports = function ({ events }) {
@@ -31,8 +67,16 @@ module.exports = function ({ events }) {
     text:' fit'
   });
 
+  const ratings = stars({
+    onclick: ({ rating }) => {
+      events.emit('image:filter', { rating });
+    },
+    className: 'filter-rating'
+  });
+
   elem.appendChild(oneToOne);
   elem.appendChild(fit);
+  elem.appendChild(ratings);
 
   return { elem, style };
 };

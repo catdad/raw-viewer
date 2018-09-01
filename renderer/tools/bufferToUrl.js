fae7032d9949913f3dda55a66692f85e56d05b5d
Kiril Vatev <vatev.1@gmail.com>
2018-09-01 10:41:42 -0400

    updating bufferToUrl references

--- renderer/tools/bufferToUrl.js
+++ renderer/tools/bufferToUrl.js
@@ -1,7 +1,3 @@
-function bufferToUrl(buff) {
+module.exports = function bufferToUrl(buff) {
   return `data:image/jpeg;base64,${Buffer.from(buff).toString('base64')}`;
-}
-
-module.exports = {
-  bufferToUrl
 };

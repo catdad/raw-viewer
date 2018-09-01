e43c7c37db0b45fb423b4809ecc4873316324de0
Kiril Vatev <vatev.1@gmail.com>
2018-09-01 10:38:58 -0400

    moving dcraw code to where it belongs

--- public/filmstrip-worker.js
+++ public/filmstrip-worker.js
@@ -1,7 +1,41 @@
 /* global postMessage */
 
-const { imageUrl, imageUint8Array } = require('../renderer/tools/util.js');
+const fs = require('fs-extra');
+const dcraw = require('dcraw');
+
+const { bufferToUrl } = require('../renderer/tools/util.js');
 const log = require('../lib/log.js')('worker');
+
+async function readFileBuffer(filepath) {
+  if (Buffer.isBuffer(filepath)) {
+    return filepath;
+  }
+
+  const file = await fs.readFile(filepath);
+
+  return file;
+}
+
+async function imageUint8Array(filepath) {
+  log.time('preview');
+
+  const file = await readFileBuffer(filepath);
+
+  // read image from raw data
+  // var tiff = dcraw(file, { exportAsTiff: true });
+
+  const preview = dcraw(file, { extractThumbnail: true });
+
+  log.timeEnd('preview');
+
+  return preview;
+}
+
+async function imageUrl(filepath) {
+  const data = bufferToUrl(await imageUint8Array(filepath));
+
+  return data;
+}
 
 function exec(data) {
   function onDone(err, res) {
--- renderer/tools/util.js
+++ renderer/tools/util.js
@@ -1,66 +1,7 @@
-const fs = require('fs-extra');
-const prettyBytes = require('pretty-bytes');
-
-const dcraw = require('dcraw');
-const log = require('../../lib/log.js')('util');
-
 function bufferToUrl(buff) {
   return `data:image/jpeg;base64,${Buffer.from(buff).toString('base64')}`;
 }
 
-async function readFileBuffer(filepath) {
-  if (Buffer.isBuffer(filepath)) {
-    return filepath;
-  }
-
-  const file = await fs.readFile(filepath);
-
-  return file;
-}
-
-async function imageUint8Array(filepath) {
-  log.time('preview');
-
-  const file = await readFileBuffer(filepath);
-
-  // read image from raw data
-  // var tiff = dcraw(file, { exportAsTiff: true });
-
-  const preview = dcraw(file, { extractThumbnail: true });
-
-  log.timeEnd('preview');
-
-  return preview;
-}
-
-async function imageUrl(filepath) {
-  const data = bufferToUrl(await imageUint8Array(filepath));
-
-  return data;
-}
-
-// TODO dead code, use exiftools instead
-async function imageMeta(filepath) {
-  const file = await readFileBuffer(filepath);
-
-  log.time('meta');
-  var meta = dcraw(file, { verbose: true, identify: true });
-  log.timeEnd('meta');
-
-  return meta.trim().split('\n').reduce((memo, str) => {
-    const [ name, ...val ] = str.split(':');
-
-    memo[name.trim()] = val.join(':').trim();
-
-    return memo;
-  }, {
-    Size: prettyBytes(file.length)
-  });
-}
-
 module.exports = {
-  imageUrl,
-  imageUint8Array,
-  imageMeta,
   bufferToUrl
 };

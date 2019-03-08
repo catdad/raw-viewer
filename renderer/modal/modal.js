f54c5c46b745ba00a1c758cd73a481259e141b0b
Kiril Vatev <vatev.1@gmail.com>
2019-03-07 22:24:23 -0500

    modal renders string content

--- renderer/modal/modal.js
+++ renderer/modal/modal.js
@@ -6,17 +6,34 @@ const style = fs.readFileSync(path.resolve(__dirname, `${name}.css`), 'utf8');
 
 const log = require('../../lib/log.js')(name);
 
+const div = (className) => {
+  const el = document.createElement('div');
+  el.className = className;
+
+  return el;
+};
+
 module.exports = ({ events }) => {
-  const elem = document.createElement('div');
-  elem.className = name;
-  elem.classList.add('hidden');
-
-  const container = document.createElement('div');
-  container.className = `${name}-container`;
-
+  const elem = div(`${name} hidden`);
+  const container = div(`${name}-container scrollbar`);
   elem.appendChild(container);
 
-  events.on('modal', ({ content }) => {
+  events.on('modal', ({ content, str }) => {
+    if (str) {
+      const fragment = document.createDocumentFragment();
+
+      str.split('\n').forEach(s => {
+        const text = document.createTextNode(s);
+        const p = document.createElement('p');
+        p.appendChild(text);
+        fragment.appendChild(p);
+      });
+
+      container.appendChild(fragment);
+
+      elem.classList.remove('hidden');
+    }
+
     log.info('modal opened', content);
   });
 

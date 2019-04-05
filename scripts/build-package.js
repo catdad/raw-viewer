14ac4bc0ba661255ecdba8d679c83f74ec35daca
Kiril Vatev <vatev.1@gmail.com>
2019-04-04 21:24:29 -0400

    creating a windows zip after building the package

--- scripts/build-package.js
+++ scripts/build-package.js
@@ -1,9 +1,12 @@
 /* eslint-disable no-console */
+const { promisify } = require('util');
+const { pipeline } = require('stream');
 const path = require('path');
 const fs = require('fs-extra');
 const del = require('del');
 const root = require('rootrequire');
 const packager = require('electron-packager');
+const archiver = require('archiver');
 
 const pkg = require('../package.json');
 
@@ -31,6 +34,22 @@ const ignore = [
   './.*'
 ];
 
+const winZip = async () => {
+  console.log('Creating Windows portable zip');
+  const name = `Raw-Viewer-${pkg.version}-Windows-portable`;
+  const filepath = `dist/${name}.zip`;
+  await fs.remove(filepath);
+  await fs.ensureFile(filepath);
+
+  const archive = archiver('zip', {
+    zlib: { level: 9 } // zlib.constants.Z_BEST_COMPRESSION
+  });
+
+  archive.directory(dirs.win32);
+
+  await promisify(pipeline)(archive, fs.createWriteStream(filepath));
+};
+
 (async () => {
   await fs.remove(dirs[platform]);
 
@@ -44,10 +63,14 @@ const ignore = [
     })],
     out: 'dist'
   });
+
+  if (platform === 'win32') {
+    await winZip();
+  }
 })().then(() => {
-  console.log('build complete');
+  console.log('Build complete');
 }).catch(err => {
-  console.log('build failed');
+  console.log('Build failed');
   console.error(err);
   process.exitCode = 1;
 });

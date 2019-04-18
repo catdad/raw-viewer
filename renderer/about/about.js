db5f43b7edf44529dba9e12655be23aed526a64e
Kiril Vatev <vatev.1@gmail.com>
2019-04-17 23:10:16 -0400

    adding links to important open-source projects

--- renderer/about/about.js
+++ renderer/about/about.js
@@ -10,8 +10,14 @@ const style = fs.readFileSync(path.resolve(__dirname, `${name}.css`), 'utf8');
 module.exports = ({ events }) => {
   const elem = dom.div(name);
   const title = dom.h1(`${pkg.productName || pkg.name} v${pkg.version}`);
-
   elem.appendChild(title);
+  elem.appendChild(dom.p('Powered by the fine open source projects:'));
+
+  [
+    dom.linkBlock('credit', 'DCRAW', 'https://www.cybercom.net/~dcoffin/dcraw/'),
+    dom.linkBlock('credit', 'ExifTool', 'https://www.sno.phy.queensu.ca/~phil/exiftool/'),
+    dom.linkBlock('credit', 'Electron', 'https://electronjs.org/'),
+  ].forEach(link => elem.appendChild(link));
 
   events.on('about', () => {
     events.emit('modal', { content: elem });

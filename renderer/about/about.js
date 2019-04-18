f9d787b4f30e38afabb970c0809ef51140c0a039
Kiril Vatev <vatev.1@gmail.com>
2019-04-17 22:40:41 -0400

    adding some content to the about modal

--- renderer/about/about.js
+++ renderer/about/about.js
@@ -1,13 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 
+const pkg = require('../../package.json');
+const dom = require('../tools/dom.js');
+
 const name = 'about';
 const style = fs.readFileSync(path.resolve(__dirname, `${name}.css`), 'utf8');
 
 module.exports = ({ events }) => {
+  const elem = dom.div(name);
+  const title = dom.h1(`${pkg.productName || pkg.name} v${pkg.version}`);
+
+  elem.appendChild(title);
 
   events.on('about', () => {
-    events.emit('modal', { str: 'about modal' });
+    events.emit('modal', { content: elem });
   });
 
   return { style };

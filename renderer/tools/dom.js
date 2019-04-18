8190ef6077ba920cd85ddf91a77602a9d2ca8fa5
Kiril Vatev <vatev.1@gmail.com>
2019-04-17 22:40:08 -0400

    adding text and h1 helpers

--- renderer/tools/dom.js
+++ renderer/tools/dom.js
@@ -5,6 +5,15 @@ const div = (className) => {
   return el;
 };
 
+const text = (str) => document.createTextNode(str);
+
+const h1 = (str) => {
+  const el = document.createElement('h1');
+  el.appendChild(text(str));
+
+  return el;
+};
+
 const empty = (elem) => {
   while (elem.firstChild) {
     elem.removeChild(elem.firstChild);
@@ -13,5 +22,7 @@ const empty = (elem) => {
 
 module.exports = {
   div,
+  text,
+  h1,
   empty
 };

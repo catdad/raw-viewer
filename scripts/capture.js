c0e249f55d17aeda08b36c61afe2014bcf6bd0e8
Kiril Vatev <vatev.1@gmail.com>
2019-05-20 22:50:18 -0400

    test: trying to see if I can switch to dark mode

--- scripts/capture.js
+++ scripts/capture.js
@@ -49,6 +49,10 @@ const upload = async (filepath) => {
   }
 };
 
+const exec = async (cmd, args, opts) => {
+  return await promisify(execFile)(cmd, args, opts);
+};
+
 (async () => {
   try {
     const configPath = await config.create({
@@ -60,7 +64,8 @@ const upload = async (filepath) => {
     await app.start(configPath);
     await app.waitForElementCount('.filmstrip .thumbnail', 1);
 
-    await promisify(execFile)('screencapture', ['-x', 'screen.jpg'], {
+    await exec('oscript', ['-e', 'tell app "System Events" to tell appearance preferences to set dark mode to not dark mode']);
+    await exec('screencapture', ['-x', 'screen.jpg'], {
       cwd: path.resolve(root)
     });
 

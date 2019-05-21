05b7ef6a5e67d1450e5aeb1f32ae2e2aceac9f68
Kiril Vatev <vatev.1@gmail.com>
2019-05-21 18:56:53 -0400

    adding new upload lib to the scripts

---
+++ scripts/lib.upload.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-console */
+const path = require('path');
+
+const fs = require('fs-extra');
+const fetch = require('node-fetch');
+const FormData = require('form-data');
+
+const fileIo = async (filepath, name = null) => {
+  const url = 'https://file.io';
+  const filename = name || path.basename(filepath);
+  console.log(`Uploading ${filepath} to ${url}`);
+
+  try {
+    const form = new FormData();
+    form.append('file', fs.createReadStream(filepath), {
+      filename: filename
+    });
+
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: form.getHeaders(),
+      body: form
+    });
+
+    const txt = await res.text();
+
+    try {
+      const json = JSON.parse(txt);
+
+      if (json.success) {
+        console.table(json);
+      } else {
+        throw new Error('not successful');
+      }
+    } catch (e) {
+      console.log(`upload failed with ${res.statusCode} and body:`);
+      console.log(txt);
+    }
+  } catch (e) {
+    console.log('upload failed with error:');
+    console.log(e);
+  }
+};
+
+module.exports = { fileIo };

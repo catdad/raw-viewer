2f9dae5f7a0b8323bb27d43c0fefff3f6ff7ac0b
Kiril Vatev <vatev.1@gmail.com>
2019-05-25 14:15:00 -0400

    using dom library for everything

--- renderer/about/about.js
+++ renderer/about/about.js
@@ -6,33 +6,29 @@ const dom = require('../tools/dom.js');
 const appName = pkg.productName || pkg.name;
 
 module.exports = ({ events }) => {
-  const elem = dom.div(name);
-  const head = dom.div('head');
-  const body = dom.div('body');
-  const foot = dom.div('foot');
-
-  [head, body, foot].forEach(el => elem.appendChild(el));
-
-  const title = dom.h1(`${appName} v${pkg.version}`);
-  head.appendChild(title);
-
-  body.appendChild(dom.p('Powered by the fine open source projects:'));
-
-  [
-    dom.linkBlock('credit', 'Electron', 'https://electronjs.org/'),
-    dom.linkBlock('credit', 'ExifTool', 'https://www.sno.phy.queensu.ca/~phil/exiftool/'),
-    dom.linkBlock('credit', 'DCRAW', 'https://www.cybercom.net/~dcoffin/dcraw/'),
-  ].forEach(link => body.appendChild(link));
-
-  dom.children(
-    foot,
+  const elem = dom.children(
+    dom.div(name),
     dom.children(
-      dom.div('byline'),
-      dom.link(appName, 'https://github.com/catdad/raw-viewer'),
-      dom.text(' is made with '),
-      dom.icon('favorite'),
-      dom.text(' by '),
-      dom.link('catdad', 'https://github.com/catdad')
+      dom.div('head'),
+      dom.h1(`${appName} v${pkg.version}`)
+    ),
+    dom.children(
+      dom.div('body'),
+      dom.p('Powered by the fine open source projects:'),
+      dom.linkBlock('credit', 'Electron', 'https://electronjs.org/'),
+      dom.linkBlock('credit', 'ExifTool', 'https://www.sno.phy.queensu.ca/~phil/exiftool/'),
+      dom.linkBlock('credit', 'DCRAW', 'https://www.cybercom.net/~dcoffin/dcraw/'),
+    ),
+    dom.children(
+      dom.div('foot'),
+      dom.children(
+        dom.div('byline'),
+        dom.link(appName, 'https://github.com/catdad/raw-viewer'),
+        dom.text(' is made with '),
+        dom.icon('favorite'),
+        dom.text(' by '),
+        dom.link('catdad', 'https://github.com/catdad')
+      )
     )
   );
 

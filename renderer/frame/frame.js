605c2c641118c21a31fef4c4c6663a3364779269
Kiril Vatev <vatev.1@gmail.com>
2019-05-26 13:32:59 -0400

    opening and closing the app menu as a context menu when

--- renderer/frame/frame.js
+++ renderer/frame/frame.js
@@ -3,9 +3,13 @@ const { BrowserWindow } = require('electron').remote;
 const name = 'frame';
 const style = true;
 
+const log = require('../../lib/log.js')(name);
+const menu = require('../../lib/menu.js');
 const dom = require('../tools/dom.js');
 
 module.exports = () => {
+  let menuOpen = false;
+
   const elem = dom.children(
     dom.div(name),
     dom.click(dom.icon('close'), () => {
@@ -22,6 +26,20 @@ module.exports = () => {
     }),
     dom.click(dom.icon('minimize'), () => {
       BrowserWindow.getFocusedWindow().minimize();
+    }),
+    dom.click(dom.icon('reorder'), (e) => {
+      const rect = e.target.getBoundingClientRect();
+
+      menu[menuOpen ? 'closeContext' : 'openContext']({
+        x: rect.left,
+        y: rect.bottom
+      }).then(() => {
+        log.info('opened frameless app menu');
+      }).catch(err => {
+        log.error('frameless app menu error', err);
+      });
+
+      menuOpen = !menuOpen;
     })
   );
 

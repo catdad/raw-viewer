34314b6fedf625ffb2b8e14e21e2aa4b008deab5
Kiril Vatev <vatev.1@gmail.com>
2019-05-26 13:02:40 -0400

    cleanup frame menu creation

--- renderer/frame/frame.js
+++ renderer/frame/frame.js
@@ -6,27 +6,23 @@ const style = true;
 const dom = require('../tools/dom.js');
 
 module.exports = () => {
-  const close = dom.click(dom.icon('close'), () => {
-    BrowserWindow.getFocusedWindow().close();
-  });
-  const maximize = dom.click(dom.icon('flip_to_front'), () => {
-    const browser = BrowserWindow.getFocusedWindow();
-
-    if (browser.isMaximized()) {
-      browser.unmaximize();
-    } else {
-      browser.maximize();
-    }
-  });
-  const minimize = dom.click(dom.icon('minimize'), () => {
-    BrowserWindow.getFocusedWindow().minimize();
-  });
-
   const elem = dom.children(
     dom.div(name),
-    close,
-    maximize,
-    minimize,
+    dom.click(dom.icon('close'), () => {
+      BrowserWindow.getFocusedWindow().close();
+    }),
+    dom.click(dom.icon('flip_to_front'), () => {
+      const browser = BrowserWindow.getFocusedWindow();
+
+      if (browser.isMaximized()) {
+        browser.unmaximize();
+      } else {
+        browser.maximize();
+      }
+    }),
+    dom.click(dom.icon('minimize'), () => {
+      BrowserWindow.getFocusedWindow().minimize();
+    })
   );
 
   return { elem, style };

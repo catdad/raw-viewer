d23455b39873078705d8ecfc95ee958bf0148226
Kiril Vatev <vatev.1@gmail.com>
2019-06-14 11:22:39 -0400

    adding dataUrl to buffer conversion

--- renderer/tools/bufferToUrl.js
+++ renderer/tools/bufferToUrl.js
@@ -1,3 +1,12 @@
 module.exports = function bufferToUrl(buff) {
   return `data:image/jpeg;base64,${Buffer.from(buff).toString('base64')}`;
 };
+
+// TODO clean up so that both are exported as named functions
+// TODO this needs to be used throughout other parts of the app
+// that convert data urls to buffer
+module.exports.reverse = (url) => {
+  const base64 = url.split(';base64,').pop();
+  const buffer = Buffer.from(base64, 'base64');
+  return buffer;
+};

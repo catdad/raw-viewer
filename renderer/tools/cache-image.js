088352fe6db09ba55385e3e1f8cccddd1818c009
Kiril Vatev <vatev.1@gmail.com>
2019-06-24 23:54:27 -0400

    allowing caching of various keys

--- renderer/tools/cache-image.js
+++ renderer/tools/cache-image.js
@@ -6,16 +6,16 @@ const electron = require('electron');
 const app = electron.app || electron.remote.app;
 
 const log = require('../../lib/log.js')('cache-image');
-const watcher = require('./cache-watcher.js');
 const noOverlap = require('./promise-overlap.js')();
 
 const dir = path.join(app.getPath('temp'), app.getName());
-const hash = (str) => crypto.createHash('md5').update(str).digest('hex');
+log.info('caching images in', dir);
 
-const file = async (filepath) => {
+const hash = (str) => crypto.createHash('md5').update(str).digest('hex');
+const file = async (filepath, key = '') => {
   // get timestamp for the file's last modification
   const { mtime } = await fs.stat(filepath);
-  return path.resolve(dir, hash(`${mtime}--{filepath}`));
+  return path.resolve(dir, hash(`${mtime}-${key}-{filepath}`));
 };
 
 const silentTiming = async (title, func) => {
@@ -24,13 +24,14 @@ const silentTiming = async (title, func) => {
       return await func();
     } catch (e) {
       log.error('ignoring image cache error:', e);
+      return null;
     }
   });
 };
 
-const read = noOverlap(async (filepath) => {
-  return await silentTiming(`reading cached ${filepath}`, async () => {
-    const location = await file(filepath);
+const read = noOverlap(async (filepath, key) => {
+  return await silentTiming(`reading cached ${key} ${filepath}`, async () => {
+    const location = await file(filepath, key);
 
     try {
       return await fs.readFile(location);
@@ -40,16 +41,16 @@ const read = noOverlap(async (filepath) => {
   });
 });
 
-const add = noOverlap(async (filepath, data) => {
-  await silentTiming(`caching ${filepath}`, async () => {
-    const location = await file(filepath);
+const add = noOverlap(async (filepath, key, data) => {
+  await silentTiming(`caching ${key} ${filepath}`, async () => {
+    const location = await file(filepath, key);
     await fs.outputFile(location, data);
   });
 });
 
-const remove = noOverlap(async (filepath) => {
-  await silentTiming(`removing ${filepath}`, async () => {
-    const location = await file(filepath);
+const remove = noOverlap(async (filepath, key) => {
+  await silentTiming(`removing ${key}, ${filepath}`, async () => {
+    const location = await file(filepath, key);
     await fs.unlink(location);
   });
 });
@@ -59,7 +60,5 @@ const reset = noOverlap(async () => {
     await fs.emptyDir(dir);
   });
 });
-
-watcher.on('change', filepath => remove(filepath));
 
 module.exports = { read, add, remove, reset };

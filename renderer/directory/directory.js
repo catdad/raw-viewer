6a07670f71d184a9f808951e5beb7533ce421231
Kiril Vatev <vatev.1@gmail.com>
2019-07-01 19:17:35 -0400

    sending filetype analytics

--- renderer/directory/directory.js
+++ renderer/directory/directory.js
@@ -2,6 +2,8 @@ const path = require('path');
 const fs = require('fs-extra');
 const { dialog } = require('electron').remote;
 const exiftool = require('../tools/exiftool-child.js');
+const collection = require('../tools/collection.js');
+const analytics = require('../../lib/analytics.js');
 const config = require('../../lib/config.js');
 
 //  const name = 'directory';
@@ -13,19 +15,28 @@ module.exports = ({ events }) => {
     await config.setProp('client.lastDirectory', dir);
     const list = (await fs.readdir(dir)).sort((a, b) => a.localeCompare(b));
 
-    const files = list.map((file) => {
-      const type = path.extname(file).toLowerCase().replace(/^\./, '');
+    const files = await collection.map(list, async (file) => {
+      const filepath = path.resolve(dir, file);
+      const type = path.extname(file).toLowerCase().replace(/^\./, '').toLowerCase();
+      const stat = await fs.stat(filepath);
 
       return {
-        filepath: path.resolve(dir, file),
-        file: file,
-        type: type
+        filepath, file, type,
+        isDir: stat.isDirectory(),
+        isFile: stat.isFile()
       };
     });
 
     return {
       dir, files
     };
+  }
+
+  function recordAnalytics({ files }) {
+    files.forEach(file => {
+      const ext = file.isDir ? 'directory' : file.type;
+      analytics.event('filetype', ext);
+    });
   }
 
   events.on('directory:open', () => {
@@ -41,7 +52,10 @@ module.exports = ({ events }) => {
   });
 
   events.on('directory:load', ({ dir }) => {
-    ondir(dir).then((result) => {
+    ondir(dir).then(result => {
+      recordAnalytics(result);
+      return result;
+    }).then((result) => {
       events.emit('directory:replace', result);
       events.emit('directory:discover', result);
     }).catch((err) => {

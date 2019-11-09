e955231d8414d85988f843ef87c048dc01885a09
Kiril Vatev <vatev.1@gmail.com>
2019-11-09 16:57:27 -0500

    adding icon and a script to deal with it

---
+++ scripts/lib.icon.js
@@ -0,0 +1,48 @@
+const { promisify } = require('util');
+const path = require('path');
+const zlib = require('zlib');
+const fs = require('fs');
+const root = require('rootrequire');
+const { createCanvas, loadImage } = require('canvas');
+const pngToIco = require('png-to-ico');
+
+const name = path.resolve(root, 'assets/icon.svgz');
+
+const read = promisify(fs.readFile);
+const write = promisify(fs.writeFile);
+
+const unzip = async name => promisify(zlib.unzip)(await read(name));
+
+//function compress() {
+//  fs.createReadStream('./assets/icon-1000.svg')
+//    .pipe(zlib.createGzip({ level: 9 }))
+//    .pipe(fs.createWriteStream(name));
+//}
+
+async function render(svg, size) {
+  const img = await loadImage(svg);
+  const canvas = createCanvas(size, size);
+  canvas.getContext('2d').drawImage(img, 0, 0);
+
+  return await canvas.toBuffer('image/png');
+}
+
+async function createIco(png) {
+  return await pngToIco(png);
+}
+
+async function prepare() {
+  const svgBuffer = await unzip(name);
+  await write(path.resolve(root, 'dist/icon.svg'), svgBuffer);
+
+  const png = await render(svgBuffer, 512);
+  await write(path.resolve(root, 'dist/icon.png'), png);
+
+  const ico = await createIco(png);
+  await write(path.resolve(root, 'dist/icon.ico'), ico);
+}
+
+prepare()
+  .then(() => console.log('done'))
+  .catch(e => console.error(e));
+

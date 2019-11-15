ad0a51d6ba91f71fa0b85e67feaef6c4d9203b55
Kiril Vatev <vatev.1@gmail.com>
2019-11-14 19:56:40 -0500

    setting the icon on the window for linux

--- main.js
+++ main.js
@@ -90,6 +90,10 @@ function createWindow () {
       windowOptions.titleBarStyle = 'hidden';
     }
 
+    if (process.platform === 'linux') {
+      windowOptions.icon = './third-party/icon.png';
+    }
+
     // Create the browser window.
     mainWindow = new BrowserWindow(windowOptions);
 
